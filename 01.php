04334af0a7ab0f4129593e6a727c0762b1e9e921
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-10 11:58:40 -0300

    Create 01.php

---
+++ 01.php
@@ -0,0 +1,32 @@
+<?php
+
+// ISSO É UM COMENTÁRIO// 
+
+$saldo = 100;
+$valorParaSacar = 150;
+
+$resultado = $valorParaSacar > $saldo;
+
+
+
+//echo($saldo);
+//echo "\n";
+
+echo ($saldo . "\n");
+
+var_dump($saldo);
+
+echo ($resultado); 
+
+echo "\n";
+var_dump($resultado);
+
+
+
+
+/**
+ echo é a instrução que envia para a saída qualquer informação, podendo conter texto, números ou variáveis
+
+var_dump debugar é inspecionar o código, tipo e conteúdo
+'' lê como string
+*/
